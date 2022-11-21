036a03d325443700d0758cbe2e31e26e0db71001
Hamidreza Ramezani <sena.hamidreza@gmail.com>
2022-11-21 22:12:31 +0330

    some work on MondoTalk Model

--- lib/MondoTalk/Common/MondoTalkModel.php
+++ lib/MondoTalk/Common/MondoTalkModel.php
@@ -4,7 +4,16 @@ namespace MondoTalk\Common;
 
 use MondoTalk\Validation\JsonValidator;
 
+
 class MondoTalkModel
 {
+    private $_propMap = array();
+
+    protected static $credential;
+
+    public static function setCredential($credential)
+    {
+        self::$credential = $credential;
+    }
 
 }
